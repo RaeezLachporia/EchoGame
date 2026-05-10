d5417830c14aa90ab224137d21f4b213c867190b
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2026-05-10 08:56:19 +0200

    Basic Movement
    Started Adding Basic Movement To The Project, As Well As Importing PlaceHolder Assets

---
+++ EchoGame0.00001/Assets/PlayerMovement/InputManager.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+   private PlayerControls playerControls;
+   public Vector2 movementInput;
+
+   private void OnEnable()
+   {
+      if (playerControls == null)
+      {
+         playerControls = new PlayerControls();
+         
+         playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+      }
+      
+      playerControls.Enable();
+   }
+
+   private void OnDisable()
+   {
+      playerControls.Disable();
+   }
+}
