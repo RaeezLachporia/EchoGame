65a62008f892dea73e6b10bbc53c614bcf8c1e70
Benjamin <itzthewildboi@gmail.com>
2026-08-07 19:10:31 +0200

    Settings menu

---
+++ EchoGame0.00001/Assets/Scripts/UIMenu's/Main Menu/Settingsz/Settings.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField] private GameObject settingspanel;
+    //[SerializeField] private CanvasGroup settingsPanel;
+
+    private void Awake()
+    {
+        //settingsPanel.alpha = 0f;
+        if(settingspanel != null)
+        {
+            settingspanel.SetActive(false);
+        }
+    }
+
+    public void OpenSettings()
+    {
+        if (settingspanel != null)
+        {
+            settingspanel.SetActive(true);
+        }
+    }
+
+    public void CloseSettings()
+    {
+        if (settingspanel != null)
+        {
+            settingspanel.SetActive(false);
+        }
+    }
+
+    public void ToggleSettings()
+    {
+        if (settingspanel != null)
+        {
+            settingspanel.SetActive(!settingspanel.activeSelf);
+        }
+    }
+    
+}
